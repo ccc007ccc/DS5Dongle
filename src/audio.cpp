dbf77e3c710ce7bd62d1f18d6445ced7bd5b5e3b
awalol <awalolcn@gmail.com>
2026-06-06 21:18:09 +0800

    add MPL License in audio.cpp

--- src/audio.cpp
+++ src/audio.cpp
@@ -2,6 +2,12 @@
 // Created by awalol on 2026/3/5.
 //
 
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #include "audio.h"
 #include "bt.h"
 #include "resample.h"
